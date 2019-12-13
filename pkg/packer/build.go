5b47fdb0b0443fdb885007e80cc56ed09d03b25a
Vaughn Dice <vadice@microsoft.com>
2019-12-12 17:50:05 -0700

    ref(build.go): update Dockerfile install lines
    Signed-off-by: Vaughn Dice <vadice@microsoft.com>

--- pkg/packer/build.go
+++ pkg/packer/build.go
@@ -1,20 +1,17 @@
 package packer
 
-// This is an example. Replace thie following with whatever steps are needed to
-// install required components into
-// const dockerfileLines = `RUN apt-get update && \
-// apt-get install gnupg apt-transport-https lsb-release software-properties-common -y && \
-// echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ stretch main" | \
-//    tee /etc/apt/sources.list.d/azure-cli.list && \
-// apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
-// 	--keyserver packages.microsoft.com \
-// 	--recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF && \
-// apt-get update && apt-get install azure-cli
-// `
+import "fmt"
+
+const packerVersion = "1.4.5"
+const dockerfileLines = `ENV PACKER_VERSION=%s
+RUN apt-get update && apt-get install -y gpg curl unzip && \
+	curl -Os https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip && \
+	unzip packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/bin
+`
 
 // Build will generate the necessary Dockerfile lines
 // for an invocation image using this mixin
 func (m *Mixin) Build() error {
-	//fmt.Fprintf(m.Out, dockerfileLines)
+	fmt.Fprintf(m.Out, dockerfileLines, packerVersion)
 	return nil
 }
